67d90d14f739e12f0b5608c24b1db2e6552036ce
luanhsd <luan_h.s.d@hotmail.com>
2020-06-22 04:29:03 -0300

    map

--- src/data/sidebar-nav-items.js
+++ src/data/sidebar-nav-items.js
@@ -3,7 +3,7 @@ export default function() {
     {
       title: 'Dashboard',
       to: '/dashboard',
-      htmlBefore: '<i class="material-icons">devices</i>',
+      htmlBefore: '<i class="material-icons">home</i>',
       htmlAfter: '',
     },
     {
@@ -47,13 +47,7 @@ export default function() {
       to: '/schedule',
       htmlBefore: '<i class="material-icons">assignment_turned_in</i>',
     },
-    /* {
-       title: "Dashboard",
-       to: "/dashboard",
-       htmlBefore: '<i class="material-icons">home</i>',
-       htmlAfter: ""
-     },
-     {
+    /*{
        title: "Forms & Componentes",
        htmlBefore: '<i class="material-icons">view_module</i>',
        to: "/components-overview",
